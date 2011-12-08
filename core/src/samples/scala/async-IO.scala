6ac9359c84d36511ec9e0037f12b742cdb77a070
Jesse Eichar <jesse.eichar@camptocamp.com>
2011-12-08 08:50:53 +0100

    added composable future concept

--- core/src/samples/scala/async-IO.scala
+++ core/src/samples/scala/async-IO.scala
@@ -31,9 +31,24 @@ object AsyncIO {
 	  val file2 = Resource.fromFile("/tmp/file2")
 	  val file3 = Resource.fromFile("/tmp/file3")
 
-	  in.async(_.copyDataTo(file1)).
+	  // this example copies the url to file1 then from file1 to file2 and file3.  
+	  // when composedFuture1 is complete all the copies are done
+	  val composedFuture1 = in.async(_.copyDataTo(file1)).
 	      flatMap(_ => file1.async(_.copyDataTo(file2))).
 	      flatMap(_ => file1.async(_.copyDataTo(file3)))
+	      
+	  composedFuture1 onComplete (_ => println("finished copying"))
+	  
+	  // this example does the same thing except the future will contain the 3
+	  // files that contain the copies (file1,file2,file3).   
+	  val composedFuture2 = for{
+	    file1 <- in.async{in => in.copyDataTo(file1); file1}
+	    file2 <- file1.async{f1 => f1.copyDataTo(file2); file2}
+	    file3 <- file1.async{f1 => f1.copyDataTo(file3); file3}
+	  } yield (file1,file2,file3)
+
+	  
+	  
 	}
 	
 }
